34b5c5a4cf4d10f20a48d7d16aa7297e5a3158a7
thequintillo <thequintillo@gmail.com>
2022-06-18 07:44:52 +0200

    auth pass-google/session

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,31 @@
 import 'module-alias/register';
 import express from 'express';
 import morgan from 'morgan';
-import Path from 'path';
-import Fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+import router from '@routes/RoutesWeb';
+import cors from 'cors';
+import { corsOptions } from '@controllers/CorsOptions.controller';
 
 const app = express();
-const path = Path;
-const fs = Fs;
-
-// MORGAN LOGS
-app.use(morgan('combined'));
+const env = dotenv.config();
+dotenvExpand.expand(env);
 
 // PORT
 const PORT = process.env.PORT || 3000;
+
+// MORGAN LOGS
+app.use(morgan('combined'));
 
 // STATIC FILES
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 // ROUTES
-app.use('/', require('./routes/RoutesWeb'));
-app.use('/register', require('./routes/RoutesWeb'));
-app.use('/*', require('./routes/RoutesWeb'));
-
+app.use('/',router);
+app.use('/register', router);
+app.use('/*', router);
 
 // SERVER ON
+app.use(cors(corsOptions));
 app.listen(PORT, () => {console.log(`SERVER ON PORT ${PORT}`)});
